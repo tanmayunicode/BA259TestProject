93830fae67c56984716e7347c943b7c060ebd408
tanmayshah <tanmayshah@192.168.29.162>
2024-09-15 10:16:47 +0530

    TestCase1 has been modified and TestCase2 has been created

--- src/testPackage/TestCase1.java
+++ src/testPackage/TestCase1.java
@@ -5,6 +5,7 @@ public class TestCase1 {
 	public static void main(String[] args) {
 
 		System.out.println("Hello...This is TestCase 1");
+		System.out.println("Hello");
 	}
 
 }
---
+++ src/testPackage/TestCase2.java
@@ -0,0 +1,10 @@
+package testPackage;
+
+public class TestCase2 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello...This is TestCase 2");
+
+	}
+
+}
